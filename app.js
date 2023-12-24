f97e2771da2db1e8c18cd003cf4f1ae4affe1744
Emir <Emir@MBP-of-Emir.cust.communityfibre.co.uk>
2023-12-24 17:18:17 +0000

    final chatapp

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const { v4: uuidV4 } = require('uuid');
-const port = 3000;
+const io = require('socket.io')(3000);
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`);
-})
-
-app.get('/:room', (req, res) => {
-    res.render('room', {roomId: req.params.room});
-});
+const users = {};
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-      socket.join(roomId);
-      socket.broadcast.to(roomId).emit('user-connected', userId); // sends to everyone excpet emitter
-
-      socket.on('disconnect', () => {
-        socket.broadcast.to(roomId).emit('user disconnected', userId);
-      })
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
     });
-});
-
-server.listen(port, () => {
-    console.log('app running on port 3000');
+    socket.on('send-chat-message', data => {
+        socket.broadcast.emit('chat-message', { message: data, name: users[socket.id] });
+    });
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
+    })
 });
